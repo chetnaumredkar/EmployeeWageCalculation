7ab303bd6a71b3392ebaf24a27d3d9d3f23a1d3e
kolhechetna <kolhe.chetna@gmail.com>
2022-10-30 17:27:28 +0530

    Added:Calculate Wage till condition meet

--- src/com/bridgelabz/ComputationOfEmployeeWage.java
+++ src/com/bridgelabz/ComputationOfEmployeeWage.java
@@ -7,9 +7,11 @@ public class ComputationOfEmployeeWage {
 		int IS_FULL_TIME = 1;
 		int IS_PART_TIME = 2;
 		int empHrs = 0;
+		int TempHrs=0;
 		int empWage = 0;
 		int Total_Working_Day=20;
 		int Total_Income;
+		int count=0;
 		for(int i=0;i<20;i++) {
 		int empcheck =(int) ((Math.random()*100) % 3);
 		switch(empcheck)  
@@ -25,12 +27,16 @@ public class ComputationOfEmployeeWage {
                 break;
         }
 		
-		empWage+=  (empHrs * Emp_Rate_Per_Hour);
+		TempHrs+=empHrs;
+		if(empHrs==16 || empHrs==8) {
+			count++;
 		}
-		System.out.println("Daily emp wage: " + empWage);
-		//Total_Income=empWage*Total_Working_Day;			
-		//System.out.println("The total income of an employee in a month is :"+Total_Income+"RS");
-			
-		
+		}
+		System.out.println(TempHrs);
+		System.out.println(count);
+		if(TempHrs>100 || count>20) {
+			empWage=TempHrs*count;
+		System.out.println("The total income of an employee in a month is " + empWage);
+		}	
+		}
 	}
-}
